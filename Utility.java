dfca5f568f0b525edbc20c8c66509198db7962c9
pranav21999 <pranavdani99@gmail.com>
2021-06-19 01:39:28 +0530

    Utility.java added

---
+++ Utility.java
@@ -0,0 +1,23 @@
+package logical_programs;
+import java.util.*;
+public class Utility 
+{
+	private static final Scanner sc =new Scanner(System.in);
+	public static double getrandomdouble()
+	{
+		return Math.random();
+	}
+	
+	public static int getuserint()
+	{
+		return sc.nextInt();
+	}
+	
+	public static int getuserchar()
+	{
+		return sc.next().charAt(0);
+	}
+	
+	
+
+}
